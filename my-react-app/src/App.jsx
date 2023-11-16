cb9c23d4bd712cc4cd2b033138ed03ab198069d2
keymice <dikifli@yahoo.com>
2023-11-16 11:32:14 +0700

    others

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,27 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <h1>Learning React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Search the docs online for more informations.
+      </p>
+    </>
+  );
+}
+
+export default App;
